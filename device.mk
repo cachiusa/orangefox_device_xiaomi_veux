df07a97a0660b36bbe8faa8c2ed2b5fac8d52a05
Alex Deymo <deymo@google.com>
2016-04-21 17:30:43 -0700

    veux: Build A/B update engine
    Change-Id: I9dbf84e1531ed3e32b21c8fec13266cfe4d23cd3

--- device.mk
+++ device.mk
@@ -63,3 +63,9 @@ PRODUCT_PACKAGES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
