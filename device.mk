e9ba270da8a1c935b0b0de08766d9c803d2b7f47
Leo Liou <leoliou@google.com>
2020-01-14 17:59:51 +0800

    veux: Inherit the virtual A/B config
    For devices launching with virtual A/B, inherit the virtual A/B
    device base configuration.
    
    Bug: 146466421
    Test: Build and boot & manual OTA.
    Change-Id: I752178c2525335b3cebafa0254c077d93831b8ba

--- device.mk
+++ device.mk
@@ -7,12 +7,13 @@
 
 LOCAL_PATH := device/unknown/veux
 # A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-    
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
