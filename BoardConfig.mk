002869dcaeb875568cddb448ab59980c74a19eaf
Alexander Koskovich <zvnexus@outlook.com>
2021-10-12 15:40:02 +0000

    veux: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     androidboot.usbcontroller=4e00000.dwc3 \
