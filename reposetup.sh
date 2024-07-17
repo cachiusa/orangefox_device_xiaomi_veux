ddd4659be576d744063eb4a74d6228a18bafeb40
cachiusa <nhat.dogpro@outlook.com>
2024-07-17 08:50:14 +0700

    Update setup script

--- reposetup.sh
+++ reposetup.sh
@@ -1,19 +1,30 @@
 check_submodule () {
-  if ! [[ -d "$2"/.git ]] ; then
-    echo "[-] Setting up: $2"
-    rm -rf $2/
-    if git clone $1 $2 --depth 1 ; then
-      echo "[OK] $2 @ $(git -C $2 rev-parse --verify --short=8 HEAD)"
+    if [[ -d "$2"/.git ]]; then
+        remote="$(git -C $2 config --get remote.origin.url)"
+        if [[ "$remote" == $1* ]]; then
+            needs_setup=0
+            ref=$(git -C $2 rev-parse --verify --short=8 HEAD)
+            echo "[  OK  ] $2 @ $ref"
+        else
+            needs_setup=1
+        fi
     else
-      echo "[FAILED] Unable to clone $1"
-      echo "[INFO] You should verify it in $(pwd)/device/xiaomi/veux/reposetup.sh"
+        needs_setup=1
     fi
-  else
-    ref=$(git -C $2 rev-parse --verify --short=8 HEAD)
-    echo "[OK] $2 @ $ref"
-  fi
+    
+    if [[ "$needs_setup" == "1" ]]; then
+        echo "[ INFO ] Setting up: $2"
+        rm -rf $2/
+        if git clone $1 $2 --depth 1 ; then
+            echo "[  OK  ] $2 @ $(git -C $2 rev-parse --verify --short=8 HEAD)"
+        else
+            echo "[FAILED] Unable to clone $1"
+            echo "[ INFO ] You should verify it in"
+            echo "    $(pwd)/device/xiaomi/veux/reposetup.sh"
+        fi
+    fi
 }
-echo "[+] Checking for dependencies..."
+echo "[ INFO ] Checking for dependencies..."
 check_submodule https://github.com/cachiusa/veux-prebuilts device/xiaomi/veux/prebuilts
 check_submodule https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
