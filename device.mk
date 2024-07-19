165a7789fe7e9df6c599b2d92a78adb164a2d274
cachiusa <nhat.dogpro@outlook.com>
2024-07-19 12:05:36 +0700

    [2/2] veux: Bring up FBEv2 decryption
    Signed-off-by: cachiusa <nhat.dogpro@outlook.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,17 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+# Encryption
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
+
+# ION
+PRODUCT_PACKAGES += \
+    libion.recovery
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
