xiaomi-sm6375-devs/android_device_xiaomi_veux

veux: Initial commit
Change-Id: Ic821c56f3b7aea42e021144aa75f9ce31301e1fd

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_veux.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_veux-user \
+    lineage_veux-userdebug \
+    lineage_veux-eng

cachiusa/orangefox_device_xiaomi_veux

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_veux.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_veux-user \
+    twrp_veux-userdebug \
+    twrp_veux-eng
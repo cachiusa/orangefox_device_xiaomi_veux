26165af09c8c10621713c02733e739836c6ac9db
Alexander Winkowski <dereference23@outlook.com>
2023-08-08 06:00:10 +0000

    veux: Initial commit
    Change-Id: Ic821c56f3b7aea42e021144aa75f9ce31301e1fd

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_veux.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_veux-user \
+    twrp_veux-userdebug \
+    twrp_veux-eng
---
+++ twrp_veux.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_BRAND := Redmi
+PRODUCT_DEVICE := veux
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2201116SG
+PRODUCT_NAME := twrp_veux
+PRODUCT_SYSTEM_NAME := veux_global
