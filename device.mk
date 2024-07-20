16e08c38978fb8259609c0de79f17f81483cd38b
cachiusa <nhat.dogpro@outlook.com>
2024-07-20 12:43:09 +0700

    rootdir: Drop unused files for recovery

--- device.mk
+++ device.mk
@@ -52,13 +52,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.default \
-    fstab.default.vendor_ramdisk \
-    init.kernel.post_boot-blair.sh \
-    init.qcom.rc \
-    init.qti.kernel.rc \
-    init.recovery.qcom.rc \
-    init.target.rc \
-    ueventd.qcom.rc
+    fstab.default.vendor_ramdisk
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
